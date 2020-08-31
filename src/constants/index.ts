9f07361c8fd87e40f4c53361d6756cc92b95e304
Andriy Chemerynskiy <andrzej.chem@gmail.com>
2020-08-31 17:03:45 +0200

    Add basic logic

---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export {default as sealGifs} from './sealGifs';
---
+++ src/constants/sealGifs.ts
@@ -0,0 +1,20 @@
+/**
+ * A static list of seal gifs so we
+ * don't need to deal with Giphy API
+ * tokens in a public action etc.
+ */
+const sealGifs: string[] = [
+  'https://media.giphy.com/media/3oKIPBdmBfMeETxnUI/giphy.gif',
+  'https://media.giphy.com/media/kEocjCo36JRgn4VjZ9/giphy.gif',
+  'https://media.giphy.com/media/dxTY4BKV7u7uLWB13S/giphy.gif',
+  'https://media.giphy.com/media/H3w8hPrt4b2mT7YchQ/giphy.gif',
+  'https://media.giphy.com/media/7JsIgnQm8JZnDtqYQu/giphy.gif',
+  'https://media.giphy.com/media/MZY7eu2dn5jm3U1dgC/giphy.gif',
+  'https://media.giphy.com/media/pjFFk8N7QLoprgFOw9/giphy.gif',
+  'https://media.giphy.com/media/Vg5EL4DJxTw4sDtpZm/giphy.gif',
+  'https://media.giphy.com/media/LPkyUiB0R7StJijXUj/giphy.gif',
+  'https://media.giphy.com/media/KfBuKHMmXFxjKKoLIH/giphy.gif',
+  'https://media.giphy.com/media/LRqvjQRIoqkIJoTKHZ/giphy.gif'
+]
+
+export default sealGifs;
